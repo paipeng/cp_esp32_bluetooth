9f7067c2919b900548d64e88c7a989302eac4675
Pai Peng <sipaipv6@gmail.com>
2023-11-01 14:20:39 +0800

    bluetooth serial to serial and back

--- cp_esp32_bluetooth.ino
+++ cp_esp32_bluetooth.ino
@@ -1,9 +1,19 @@
+#include <BluetoothSerial.h>
+
+BluetoothSerial bs;
 
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
 
+  bs.begin("ESP32-BT");
 }
 
 void loop() {
+  if (Serial.available()) {
+    bs.write(Serial.read());
+  } else if (bs.available()) {
+    Serial.write(bs.read());
+  }
+  delay(20);
 }
